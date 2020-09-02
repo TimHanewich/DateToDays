b15419e3d3f18bd51b4bfc7aa334b7cf0a440185
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2020-09-01 19:41:11 -0700

    Removed sample

--- HttpTriggerCSharp1.cs
+++ HttpTriggerCSharp1.cs
@@ -14,26 +14,7 @@ namespace DateToDays
 {
     public static class HttpTriggerCSharp1
     {
-        [FunctionName("HttpTriggerCSharp1")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
-        {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
-        }
-
+        
         [FunctionName("DateToDays")]
         public static HttpResponseMessage DateToDays([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequest request, ILogger log)
         {
